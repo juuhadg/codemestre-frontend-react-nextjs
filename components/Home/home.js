7b4a2db2cdc92fb279c9924ed8ea520d451199c4
juuhadg <juku.jj@gmail.com>
2023-10-20 21:48:55 -0300

    mudanças home

--- components/Home/home.js
+++ components/Home/home.js
@@ -2,6 +2,7 @@ import Image from 'next/image'
 import iconPergaminho from '../../public/iconPergaminho.svg'
 import iconNinja from '../../public/iconNinja.svg'
 import iconMestre from '../../public/iconMestre.svg'
+import imgNinjaGrande from '../../public/ninja.png'
 export default function Home() {
     return (
         <>
@@ -14,21 +15,40 @@ export default function Home() {
       <p>Nossos mestres estão aqui para guiá-lo em um caminho de conhecimento e maestria.</p>
       <h2>De Graça.</h2>
         </div>
+        <section className='cardImagem'>
+        
         <div className="cardInformacoes">
           <h2>Treine como um Ninja, Vire um Mestre.</h2>
           <ul>
             <li>Conteúdos em Diversas Linguagens</li>
+            <li>Não é Necessário Conhecimento Prévio</li>
             <li>Aprenda com Exemplos</li>
             <li>Problemas e Desafios ao fim de Cada Lição</li>
-            <li>Evolua a Cada Conteúdo Estudado</li>
             <li>Suba seu Nível e suas Maestrias ao solucionar Desafios</li>
           </ul>
+         <p>Linguagens Disponíveis: <br></br> C# - JavaScript - Python</p>
+        
+          
           <button>Comece A Sua Jornada</button>
         </div>
+
+        <div className="cardInformacoes">
+          <h2>Registre-se Gratuitamente e comece a Aprender<br></br> e se Aprimorar</h2>
+          <p>Ou Navegue livremente pelos conteúdos, Mas sem Salvar seu Progresso...</p>
+          <ul>
+            <li>Suba de Nível Ao Progredir</li>
+            <li>Ranks Personalizados Baseados no seu Nível</li>
+            <li>Acompanhe Seu Progresso em Cada Linguagem</li>
+            <li>Jornada Completa de Novato a Mestre</li>
+          </ul>
+         <h2> Junte-se á Jornada</h2>
+        </div>
+        
+        </section>
           <div className="cardInformacoes2">
               <h1>Aprenda, Pratique, Evolua</h1>
               <h2>Treine como um Ninja e Evolua seu Nível em Programação de Novato á um verdadeiro Mestre</h2>
-              <h1>Como Funciona?</h1>
+              <h1>Como é A Jornada?</h1>
               <div className="comoFuncionaBox">
                 <div className='item'>
                   <Image src={iconPergaminho}/>
@@ -44,7 +64,7 @@ export default function Home() {
                   <Image src={iconMestre}/>
                   <h2>Evolução</h2>
                   <p>Suba seu Nível de Habilidade a cada treinamento,<br></br> torne-se um Mestre nas suas linguagens preferidas.</p>
-                  <p>A Jornada para chegar ao topo não é fácil.<br></br> somente a dedicação cria Mestres Lendários</p>
+                  <p>A Jornada para chegar ao topo não é fácil.<br></br> somente a dedicação cria verdadeiros Mestres Lendários.</p>
                   </div>
               </div>
           </div>

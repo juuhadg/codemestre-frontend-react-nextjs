96ff5ba5e032d785f032fb9ce7d307e6b38e5c43
juuhadg <juku.jj@gmail.com>
2023-10-25 17:54:07 -0300

    login com Google

--- components/LoginPage/LoginPage.js
+++ components/LoginPage/LoginPage.js
@@ -1,19 +1,26 @@
 import { useState,useEffect } from "react"
+import { GoogleLogin } from '@react-oauth/google'
+import jwtDecode from "jwt-decode"
+
+
 
 export default function LoginPage() {
     const [nome,setNome] = useState('')
     const [email,setEmail] = useState('')
     const [senha,setSenha] = useState('')
 
+
     useEffect(() => {
        
         document.body.classList.add('login');
+       
     
         
         return () => {
           document.body.classList.remove('login');
         };
       }, []); 
+
 
 
     return (
@@ -23,20 +30,41 @@ export default function LoginPage() {
         <div className="loginContainer">
             <h1>Faça Seu Login !</h1>
                 <form onSubmit={()=>{console.log('enviado form')}}>
-                    <div>
+                   
                         <label>Nome : </label>
                         <input  className = 'inputLogin' type="text" placeholder="Nome" value={nome} onChange={e=>{setNome(e.target.value)}}/>
-                    </div>
-                    <div>
+                   
+                 
                         <label>E-mail : </label>
                         <input  className = 'inputLogin' type="text" placeholder="Email" value={email} onChange={e=>{setEmail(e.target.value)}}/>
-                    </div>
-                    <div>
+                  
+                  
                         <label>Senha : </label>
-                        <input  className = 'inputLogin' type="password" placeholder="Senha" value={senha} onChange={e=>{setSenha(e.target.value)}}/> </div>
+                        <input  className = 'inputLogin' type="password" placeholder="Senha" value={senha} onChange={e=>{setSenha(e.target.value)}}/> 
 
                     <button className="botaoEnviar" type="submit">Enviar</button>
                 </form>
+         <h1>Ou</h1>
+         <h2>Entrar com Google</h2>
+         <div className="botaoGoogle">
+         <GoogleLogin
+         size="large"
+         
+    logo_alignment="center"
+    auto_select={false}
+    useOneTap={false}
+    width={500}
+  onSuccess={credentialResponse => {
+    var credencialResposta = jwtDecode(credentialResponse.credential)
+    console.log(credencialResposta)
+  }}
+
+
+  onError={() => {
+    console.log('Login Failed');
+  }}
+
+/>;</div>
          </div>
          </div>
        
--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,9 @@ import Cabecalho from '@/components/Cabecalho/Cabecalho'
 import '@/styles/globals.scss'
 import Head from 'next/head'
 import { useRouter } from 'next/router'
+import { GoogleOAuthProvider } from "@react-oauth/google"
+
+const  GOOGLE_CLIENT_ID  = '665992785702-vkr3gn3vinvj1rhhloddlf7ivj4nkek9.apps.googleusercontent.com'
 
 
 export default function App({ Component, pageProps }) {
@@ -9,10 +12,12 @@ export default function App({ Component, pageProps }) {
   const router = useRouter()
   return ( 
     <>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
     <Head>
     <title>CodeMestre</title>
     </Head>
      {(router.asPath != '/login' && router.asPath != '/cadastro') && (<Cabecalho/>)}
   <Component {...pageProps} />
+  </GoogleOAuthProvider>
   </>
 )}

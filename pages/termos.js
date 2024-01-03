b1d65a80b76b8d1f24c2196aaf36e9b73cb2dc54
juuhadg <juku.jj@gmail.com>
2024-01-03 18:04:27 -0300

    termos

---
+++ pages/termos.js
@@ -0,0 +1,32 @@
+export default function termos() {
+    return (
+        <>
+        <div className="termos">
+         <h1>Política de Privacidade para Autenticação com Google OAuth</h1>
+
+<p><strong>1. Informações Coletadas:</strong></p>
+<p>Ao utilizar a autenticação com Google OAuth em nosso site, podemos coletar informações básicas do seu perfil do Google, como seu nome, endereço de e-mail e imagem de perfil. Essas informações são usadas exclusivamente para autenticação e personalização da sua experiência no site.</p>
+
+<p><strong>2. Uso das Informações:</strong></p>
+<p>As informações coletadas durante a autenticação serão utilizadas apenas para os fins relacionados à autenticação do usuário em nosso site. Não compartilharemos ou venderemos suas informações a terceiros.</p>
+
+<p><strong>3. Segurança:</strong></p>
+<p>Implementamos medidas de segurança para proteger suas informações durante a autenticação. Isso inclui a utilização de protocolos seguros de comunicação e o armazenamento seguro de dados.</p>
+
+<p><strong>4. Acesso e Controle:</strong></p>
+<p>Você tem o direito de acessar as informações coletadas durante a autenticação e pode solicitar a exclusão dessas informações a qualquer momento. Para exercer esses direitos, entre em contato conosco através dos meios disponibilizados no nosso site.</p>
+
+<p><strong>5. Cookies e Rastreamento:</strong></p>
+<p>Podemos utilizar cookies e outras tecnologias de rastreamento durante a autenticação para melhorar a experiência do usuário e fornecer funcionalidades personalizadas. Você pode ajustar as configurações do seu navegador para gerenciar o uso de cookies.</p>
+
+<p><strong>6. Alterações na Política de Privacidade:</strong></p>
+<p>Reservamo-nos o direito de atualizar ou modificar esta Política de Privacidade. Quaisquer alterações significativas serão notificadas aos usuários através do nosso site ou por meio de outros canais de comunicação.</p>
+
+<p><strong>7. Contato:</strong></p>
+<p>Para dúvidas ou preocupações sobre esta Política de Privacidade, entre em contato conosco através dos meios disponibilizados no nosso site.</p>
+
+<p>Ao utilizar a autenticação com Google OAuth em nosso site, você concorda com os termos desta Política de Privacidade.</p>
+</div>
+        </>
+    )
+}
